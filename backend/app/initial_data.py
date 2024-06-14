eaa398becd6d7892799765f8962ed6b0ad80c58c
Meskeremhab <meski123habtom@gmail.com>
2024-06-14 12:11:54 +0200

    initial_data changed

--- backend/app/initial_data.py
+++ backend/app/initial_data.py
@@ -2,25 +2,29 @@
 # Changes are required to switch from SQLModel to ODMantic (MongoDB).
 
 import logging
+from odmantic import AIOEngine
+from motor.motor_asyncio import AsyncIOMotorClient
+import asyncio
+from app.core.db import init_db  
 
-from sqlmodel import Session
-
-from app.core.db import engine, init_db
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+MONGODB_URI = "mongodb+srv://admin:admin1234!@cluster0.ba2wonq.mongodb.net/test-db?retryWrites=true&w=majority&appName=Cluster0"
 
-def init() -> None:
-    with Session(engine) as session:
-        init_db(session)
+# Setup the MongoDB client and engine
 
+client = AsyncIOMotorClient(MONGODB_URI)
+engine = AIOEngine(motor_client=client, database="test-db")  
 
-def main() -> None:
+async def init() -> None:
+    await init_db(engine)
+
+async def main() -> None:
     logger.info("Creating initial data")
-    init()
+    await init()
     logger.info("Initial data created")
-
-
+    
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
