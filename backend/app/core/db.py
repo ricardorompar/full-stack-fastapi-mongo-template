e9e8cf872e3b8cdc7e7fcabbd8d44ac59cbaa4aa
Meskeremhab <meski123habtom@gmail.com>
2024-06-22 10:10:30 +0200

    app/core/db.py on  async def init_db(engine:AIOengine) added

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,7 +1,7 @@
 from odmantic import AIOEngine
 from motor.motor_asyncio import AsyncIOMotorClient
-
-
+import asyncio
+#from app.db import engine
 from app import crud
 from app.core.config import settings
 from app.models import User, UserCreate
@@ -16,15 +16,28 @@ engine = AIOEngine(client=client, database=settings.MONGODB_DB)
 
 
 # Define an asynchronous function to initialize the database
-async def init_db(engine) -> None:
-    # Check if the first superuser already exists
+#async def init_db(engine) -> None:
+#    # Check if the first superuser already exists
+#    user = await crud.get_user_by_email(engine, settings.FIRST_SUPERUSER)
+#    if not user:
+#        # If the user doesn't exist, create a new UserCreate instance
+#        user_in = UserCreate(
+#            email=settings.FIRST_SUPERUSER,
+#            password=settings.FIRST_SUPERUSER_PASSWORD,
+#            is_superuser=True,
+#        )
+#        # Create the superuser in the database
+#        #await crud.create_user(engine, user_create=user_in)
+#        await crud.create_user(engine=engine, user_create=user_in)
+async def init_db(engine: AIOEngine) -> None:
     user = await crud.get_user_by_email(engine, settings.FIRST_SUPERUSER)
     if not user:
-        # If the user doesn't exist, create a new UserCreate instance
         user_in = UserCreate(
             email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
-        # Create the superuser in the database
-        await crud.create_user(engine, user_create=user_in)
+        await crud.create_user(engine=engine, user_create=user_in)
+
+# Run the init_db function
+#asyncio.run(init_db(engine))
